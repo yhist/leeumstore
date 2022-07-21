a5a2076b8a8395350547838c0a9714b425348b8b
yhist <yhyh0903@gmail.com>
2022-07-22 01:28:43 +0900

    slide 구현

--- js/script.js
+++ js/script.js
@@ -3,6 +3,20 @@ $(document).ready(function(){
 });
 
 
+window.onload = function(){
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+}
+
+
+
 
 
 
