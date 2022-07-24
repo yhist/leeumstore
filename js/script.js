1ea7e088fa0bbe02c23825e4c34e8137aa022839
yhist <yhyh0903@gmail.com>
2022-07-24 23:27:26 +0900

    fabric까지

--- js/script.js
+++ js/script.js
@@ -10,10 +10,51 @@ window.onload = function(){
     spaceBetween: 30,
     slidesPerGroup: 3,
     navigation: {
-      nextEl: ".sw-next"
-    }
-
+      nextEl: ".sw-ceramic-next"
+  }   
   });
+
+  var swiper = new Swiper(".sw-glass", {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    navigation: {
+      nextEl: ".sw-glass-next"
+    }
+  });
+
+  var swiper = new Swiper(".sw-metal", {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    navigation: {
+      nextEl: ".sw-metal-next"
+    }
+  });
+
+  var swiper = new Swiper(".sw-paper", {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    navigation: {
+      nextEl: ".sw-paper-next"
+    }
+  });
+
+  var swiper = new Swiper(".sw-fabric", {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    navigation: {
+      nextEl: ".sw-fabric-next"
+    }
+  });
+
+
 
 }
 
