86ff56e70311984c1aa05832efa7dc92268c87ac
yhist <yhyh0903@gmail.com>
2022-08-28 22:57:11 +0900

    footer 구현

--- js/script.js
+++ js/script.js
@@ -1,12 +1,39 @@
 $(document).ready(function(){
   // 펼침메뉴 만들기
-  let sideMenu = $('.side-menu');
-  // 
+  let sideMenu = $('.side-menu-list');
+  let sideMenuLi = $('.side-menu-list>li');
+
+  // 서브메뉴 펼치기
+  $.each(sideMenuLi, function(index, item){
+    $(this).click(function(){
+      let sideMenuLiA = $(this).find('>a');
+      let subMenu = $(this).find('.sub-menu');
+      sideMenuLiA.click(function(event){
+        event.preventDefault();
+        subMenu.fadeToggle();
+      });
+
+    });
+  });
+
+  // 서브메뉴2 펼치기
+  let subMenu = $('.sub-menu>li');
+  $.each(subMenu, function(index, item){
+    let subMenuA = $(this).find('>a');
+    let subMenu_2 = $(this).find('.sub-menu-2');
+    $(this).click(function(){
+      subMenuA.click(function(event){
+        event.preventDefault();
+        subMenu_2.fadeToggle();
+      });
+    });
+  });
+
+
 });
 
 
 window.onload = function(){
-
 
   var swiper = new Swiper(".sw-ceramic", {
     loop: true,
