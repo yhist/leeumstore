f5fcd82188ecf11ae2ac9312d47fdd9a78955096
yhist <yhyh0903@gmail.com>
2022-09-14 18:08:02 +0900

    메인 pc 버전 완료

--- js/script.js
+++ js/script.js
@@ -1,48 +1,40 @@
-$(document).ready(function(){
+$(document).ready(function () {
   // 펼침메뉴 만들기
-  let sideMenu = $('.side-menu-list');
-  let sideMenuLi = $('.side-menu-list>li');
+  let sideMenu = $(".side-menu-list");
+  let sideMenuLi = $(".side-menu-list>li");
 
   // 서브메뉴 펼치기
-  $.each(sideMenuLi, function(index, item){
-    $(this).click(function(){
-      let sideMenuLiA = $(this).find('>a');
-      let subMenu = $(this).find('.sub-menu');
-      sideMenuLiA.click(function(event){
-        event.preventDefault();
-        subMenu.fadeToggle();
-      });
-
+  $.each(sideMenuLi, function (index, item) {
+    let sideMenuLiA = $(this).find(">a");
+    let subMenu = $(this).find(".sub-menu");
+    sideMenuLiA.click(function (event) {
+      event.preventDefault();
+      subMenu.stop().slideToggle();
     });
   });
 
   // 서브메뉴2 펼치기
-  let subMenu = $('.sub-menu>li');
-  $.each(subMenu, function(index, item){
-    let subMenuA = $(this).find('>a');
-    let subMenu_2 = $(this).find('.sub-menu-2');
-    $(this).click(function(){
-      subMenuA.click(function(event){
-        event.preventDefault();
-        subMenu_2.fadeToggle();
-      });
+  let subMenu = $(".sub-menu>li");
+  $.each(subMenu, function (index, item) {
+    let subMenuI = $(this).find(">i");
+    let subMenu_2 = $(this).find(".sub-menu-2");
+    subMenuI.click(function (event) {
+      $(this).toggleClass("active");
+      event.preventDefault();
+      subMenu_2.stop().slideToggle();
     });
   });
-
-
 });
 
-
-window.onload = function(){
-
+window.onload = function () {
   var swiper = new Swiper(".sw-ceramic", {
     loop: true,
     slidesPerView: 3,
     spaceBetween: 30,
     slidesPerGroup: 3,
     navigation: {
-      nextEl: ".sw-ceramic-next"
-  }   
+      nextEl: ".sw-ceramic-next",
+    },
   });
 
   var swiper = new Swiper(".sw-glass", {
@@ -51,8 +43,8 @@ window.onload = function(){
     spaceBetween: 30,
     slidesPerGroup: 3,
     navigation: {
-      nextEl: ".sw-glass-next"
-    }
+      nextEl: ".sw-glass-next",
+    },
   });
 
   var swiper = new Swiper(".sw-metal", {
@@ -61,8 +53,8 @@ window.onload = function(){
     spaceBetween: 30,
     slidesPerGroup: 3,
     navigation: {
-      nextEl: ".sw-metal-next"
-    }
+      nextEl: ".sw-metal-next",
+    },
   });
 
   var swiper = new Swiper(".sw-paper", {
@@ -71,8 +63,8 @@ window.onload = function(){
     spaceBetween: 30,
     slidesPerGroup: 3,
     navigation: {
-      nextEl: ".sw-paper-next"
-    }
+      nextEl: ".sw-paper-next",
+    },
   });
 
   var swiper = new Swiper(".sw-fabric", {
@@ -81,19 +73,7 @@ window.onload = function(){
     spaceBetween: 30,
     slidesPerGroup: 3,
     navigation: {
-      nextEl: ".sw-fabric-next"
-    }
+      nextEl: ".sw-fabric-next",
+    },
   });
-
-
-
-}
-
-
-
-
-
-
-
-
-
+};
